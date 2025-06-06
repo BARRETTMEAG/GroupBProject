af6ad1e213cb15e4f87c5db88d15d6881d92095b
BARRETTMEAG <10504850@uvu.edu>
2025-06-06 17:00:16 -0600

    Update main_test.py

--- Milestone_2/main_test.py
+++ Milestone_2/main_test.py
@@ -9,6 +9,7 @@ def setup():
     memory = Memory()
     return cpu, memory
     
+# Testing files, even if it exist if not
 def test_file_open():
     sim = UVSim()
     sim.load_program('Test1.txt') #Assumes existence of Test1.txt
@@ -16,6 +17,7 @@ def test_file_open():
     with pytest.raises(OSError):
         sim.load_program('qwertyuiop.txt') #Assumes this file does not exist
 
+# Testing the inital program of Test1.txt and Test2.txt
 def test_program_init():
     sim = UVSim()
     sim.load_program('Test1.txt')
@@ -39,7 +41,8 @@ def test_program_init():
     assert sim.memory.memory[6] == 4300
     assert sim.memory.memory[7] == 1110
     assert sim.memory.memory[8] == 4300
-    
+
+# Tests writing and reading memory
 def test_memory_write_and_read():
     memory = Memory()
     memory.write(10, 1234)
@@ -51,6 +54,7 @@ def test_memory_write_and_read():
     with pytest.raises(IndexError):
         memory.read(-1)
 
+# Tests loading from stored memory
 def test_load_store(setup):
     cpu, memory = setup
     memory.write(5, 4321)
@@ -60,6 +64,7 @@ def test_load_store(setup):
     cpu.execute(2106, memory.memory)  # STORE to address 6
     assert memory.read(6) == 4321
 
+# Tests the both the addition and subtraction of memory
 def test_add_subtract(setup):
     cpu, memory = setup
     cpu.accumulator = 100
@@ -71,6 +76,7 @@ def test_add_subtract(setup):
     cpu.execute(3102, memory.memory)  # SUBTRACT from address 2
     assert cpu.accumulator == 125
 
+# Tests the multipication and division of memory
 def test_multiply_divide(setup):
     cpu, memory = setup
     cpu.accumulator = 10
@@ -82,6 +88,7 @@ def test_multiply_divide(setup):
     cpu.execute(3204, memory.memory)  # DIVIDE
     assert cpu.accumulator == 25.0
 
+# Tests memory branch operations (branch, branchneg, branchzero, and if no branching) 
 def test_branch_operations(setup):
     cpu, memory = setup
     cpu.execute(4007, memory.memory)  # BRANCH to 07
@@ -100,21 +107,25 @@ def test_branch_operations(setup):
     cpu.execute(4203, memory.memory)  # Should not branch
     assert cpu.program_counter == 1 # PC moves up one instead of branching
 
+# Tests Halt
 def test_halt(setup):
     cpu, memory = setup
     cpu.execute(4300, memory.memory)
     assert cpu.halted is True
 
+# Tests bad opcode
 def test_bad_opcode(setup):
     cpu, memory = setup
     with pytest.raises(ValueError):
         cpu.execute(5555, 8)
-    
+
+# Tests bad word length
 def test_bad_word_length(setup):
     cpu, memory = setup
     with pytest.raises(ValueError):
         memory.write(5, 12345)
 
+# Tests negative values
 def test_negative_values():
     sim = UVSim()
     memory = sim.memory
@@ -127,6 +138,7 @@ def test_negative_values():
     sim.run()
     assert sim.cpu.accumulator == 20
 
+# Tests when subtracting negatives
 def test_subtract_negative():
     sim = UVSim()
     memory = sim.memory
@@ -139,6 +151,7 @@ def test_subtract_negative():
     sim.run()
     assert sim.cpu.accumulator == 180
 
+# Tests bad word format
 def test_bad_word_format():
     sim = UVSim()
     with pytest.raises(TypeError):
@@ -148,6 +161,7 @@ def test_bad_word_format():
     with pytest.raises(TypeError):
         sim.run()
 
+# Tests improper word in file
 def test_improper_word_in_file():
     sim = UVSim()
     with pytest.raises(ValueError):
@@ -162,6 +176,7 @@ def test_branch(setup):
     cpu.execute(4005, memory.memory)
     assert cpu.program_counter == 5
 
+# Tests branching executes until halt command
 def test_branch_executes_until_halt():
     sim = UVSim()
     sim.memory.write(0, 4001)  # BRANCH to line 1
@@ -179,6 +194,7 @@ def test_write_prints_hello_world(monkeypatch, capsys, setup):
     captured = capsys.readouterr()
     assert "Hello World" in captured.out
 
+# Tests input and print hello world
 def test_input_then_print_hello_world(monkeypatch, capsys):
     sim = UVSim()
     sim.memory.write(0, 1020)  # READ into address 20
@@ -202,6 +218,7 @@ def test_load_and_store_value(setup):
     cpu.execute(2111, mem.memory)  # STORE to address 11
     assert mem.read(11) == 42
 
+# Tests what araises when invalid address submitted
 def test_invalid_address_raises():
     mem = Memory()
     with pytest.raises(IndexError):
@@ -215,6 +232,7 @@ def test_multiply_two_values(setup):
     cpu.execute(3307, mem.memory)  # MULTIPLY by memory[7]
     assert cpu.accumulator == 42
 
+# Tests Multiplication with zero
 def test_multiplication_with_zero(setup):
     cpu, mem = setup
     cpu.accumulator = 0
@@ -230,6 +248,7 @@ def test_divide_two_values(setup):
     cpu.execute(3205, mem.memory)  # DIVIDE by memory[5]
     assert cpu.accumulator == 6
 
+# Tests division by zero
 def test_division_by_zero(setup):
     cpu, mem = setup
     cpu.accumulator = 10
