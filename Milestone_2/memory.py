05b28dbb752fc3a14440883fb7033658bc36ad3e
Collin Ross <collinross@macbookair.lan>
2025-06-05 20:23:42 -0600

    updated error handling with memory

--- Milestone_2/memory.py
+++ Milestone_2/memory.py
@@ -4,9 +4,13 @@ class Memory:
 
     def write(self, address, value):
         if 0 <= address < 100:
-            self.memory[address] = value
+            if -9999 <= value <= 9999:  # Ensure value is a signed four-digit integer
+                self.memory[address] = value
+            else:
+                raise ValueError("Value must be a signed four-digit integer")
         else:
             raise IndexError("Memory address out of range")
+
 
     def read(self, address):
         if 0 <= address < 100:
