3a958ee6ccdfc2efa35d8152663e83b1b3fba609
Z-Erich <erichzaugg@gmail.com>
2025-06-12 22:40:03 -0600

    Update main.py
    Feedback fixes!

--- Milestone 3/main.py
+++ Milestone 3/main.py
@@ -1,6 +1,13 @@
 import os
 from memory import Memory
 
+def convert_to_int(word):
+    try:
+        int(word)
+        return int(word)
+    except ValueError:
+        return False
+
 class CPU:
     def __init__(self):
         self.accumulator = 0
@@ -13,28 +20,36 @@ class CPU:
 
         if opcode == 10:  # READ = Read a word from the keyboard into a specific location in memory.
             value = input("Enter the value to be stored: ")
-            memory[operand] = value
+            intValue = convert_to_int(value)
+            if intValue is False:
+                raise ValueError("Invalid entry! Please enter signed 4-digit integers only...")
+            memory.write(operand, intValue)
 
         elif opcode == 11:  # WRITE = Write a word from a specific location in memory to screen.
-            print(str(memory[operand]))
+            output = int(memory.read(operand))
+            print(str(output))
 
         elif opcode == 20:  # LOAD = Load a word from a specific location in memory into the accumulator.
-            self.accumulator = memory[operand]
+            self.accumulator = memory.read(operand)
 
         elif opcode == 21:  # STORE = Store a word from the accumulator into a specific location in memory.
-            memory[operand] = self.accumulator
+            word = self.accumulator
+            memory.write(operand, word)
 
         elif opcode == 30:  # ADD = Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)
-            self.accumulator = int(self.accumulator) + int(memory[operand])
+            self.accumulator = int(self.accumulator) + int(memory.read(operand))
 
         elif opcode == 31:  # SUBTRACT = Subtract a word from a specific location in memory from the word in the accumulator (leave the result in the accumulator)
-            self.accumulator = int(self.accumulator) - int(memory[operand])
+            self.accumulator = int(self.accumulator) - int(memory.read(operand))
 
         elif opcode == 32:  # DIVIDE = Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator).
-            self.accumulator = int(self.accumulator) / int(memory[operand])
+            divisor = int(memory.read(operand))
+            if divisor == 0:
+                raise ValueError("Memory at [" + str(operand) + "] is '0'.  Cannot divide by 0!")
+            self.accumulator = int(self.accumulator) / divisor
 
         elif opcode == 33:  # MULTIPLY = multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator).
-            self.accumulator = int(self.accumulator) * int(memory[operand])
+            self.accumulator = int(self.accumulator) * int(memory.read(operand))
 
         elif opcode == 40:  # BRANCH = Branch to a specific location in memory
             self.program_counter = operand
@@ -62,6 +77,9 @@ class CPU:
         else:
             raise ValueError("Unknown opcode '" + str(opcode) + "' encountered at memory [" + str(self.program_counter) + "]")
 
+        if self.accumulator > 9999 or self.accumulator < -9999:
+            self.accumulator = self.accumulator % 10000
+            
         self.program_counter += 1
         return
 
@@ -79,19 +97,27 @@ class UVSim:
                 while True:
                     char = file.read(1)
                     if not char:  # End of file
+                        if word and word != '\n':
+                            instruction = convert_to_int(word)
+                            if instruction is False:
+                                raise ValueError("This file contains an invalid word at memory [" + str(i) + "]. Words must be signed 4-digit integers (no spaces).")
                         break
-                    if not char.isspace():
-                        word += char
-                    elif char.isspace(): # Program encounters whitespace
-                        if not word:
-                            continue # Skips words containing only blank spaces
-                        elif len(word) != 5: # Validates length of word (including sign)
-                            raise ValueError("Incorrect word length encountered at memory [" + str(i) + "]")
-                        else:
-                            instruction = int(word) # Convert word to instruction of type integer when an 'enter' char is encountered
+                    word += char
+                    if char.isspace(): # Program encounters whitespace
+                        if char == '\n':
+                            if not word or word == '\n':
+                                raise ValueError("Empty word found at memory [" + str(i) + "]")
+                            instruction = convert_to_int(word)
+                            if instruction is False:
+                                raise ValueError("This file contains an invalid word at memory [" + str(i) + "]. Words must be signed 4-digit integers (no spaces).")
+                            if len(word) < 6:
+                                raise ValueError("All words in the file must be signed 4-digit integers.  Invalid word at memory [" + str(i) + "]")
                             self.memory.write(i, instruction)
                             i += 1
                             word = ""
+                            continue
+                        raise ValueError("Invalid space found in the word at memory [" + str(i) + "]")
+                        
         else:
             print(f"The file '{filename}' does not exist.")
             filename = input("Please enter the name of your file: ")
@@ -100,7 +126,7 @@ class UVSim:
     def run(self):
         while not self.cpu.halted:
             instruction = self.memory.read(self.cpu.program_counter)
-            self.cpu.execute(instruction, self.memory.memory)
+            self.cpu.execute(instruction, self.memory)
 
 
 def main():
