094460d2ee615700f412a16e43cdd5762c428ced
Z-Erich <erichzaugg@gmail.com>
2025-06-12 22:42:46 -0600

    Add memory.py to Milestone 3
    Added the memory file to this milestone, fixed a truncating issue (see feedback)

---
+++ Milestone 3/memory.py
@@ -0,0 +1,19 @@
+class Memory:
+    def __init__(self):
+        self.memory = [0] * 100  # Initialize memory with 100 words (00-99)
+
+    def write(self, address, value):
+        if 0 <= address < 100:
+            if -9999 <= value <= 9999:  # Ensure value is a signed four-digit integer
+                self.memory[address] = value
+            else:
+                self.memory[address] = value % 10000 # Truncate to a 4-digit integer
+        else:
+            raise IndexError("Memory address out of range")
+
+
+    def read(self, address):
+        if 0 <= address < 100:
+            return self.memory[address]
+        else:
+            raise IndexError("Memory address out of range")
