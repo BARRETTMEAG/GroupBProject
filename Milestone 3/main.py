23ed4fbedf09e486e7b6a193ce262a1d2b1262f5
Z-Erich <erichzaugg@gmail.com>
2025-06-19 22:24:31 -0600

    Update main.py

--- Milestone 3/main.py
+++ Milestone 3/main.py
@@ -1,29 +1,11 @@
-import os
-from memory import Memory
-from convert import Convert
-from cpu import CPU
-from uvsim import UVSim
+import tkinter as tk
+from uvsim_gui import UVSimGUI
 
 def main():
 
-    sim = UVSim()
-    filename = input("Please enter the name of your file: ")
-    sim.load_program(filename)
-    sim.run()
-
-    # Output code for troubleshooting:
-    """
-    j = 0
-    for i in sim.memory.memory:
-        if sim.memory.memory[j]:
-            print("Memory at [" + str(j) + "] is: ", str(sim.memory.memory[j]))
-            j += 1
-        else:
-            print("Memory at [" + str(j) + "] is empty.")
-            j += 1
-    """
-    print("Accumulator register value:", str(sim.cpu.accumulator))
-    #
+    root = tk.Tk()
+    UVSimGUI(root)
+    root.mainloop()
 
 if __name__=="__main__":
     main()
