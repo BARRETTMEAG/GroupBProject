0055d2cec638c9c03a3005482567a77bb3058b62
tess-obrian <89497269+tess-obrian@users.noreply.github.com>
2025-07-08 12:57:17 -0600

    Update convert.py to fix some bugs

--- Milestone_4 copy/convert.py
+++ Milestone_4 copy/convert.py
@@ -2,6 +2,9 @@ class Convert:
     @staticmethod
     def convert_to_int(word):
         try:
+            if isinstance(word, int):
+                return word
             return int(word.strip())
         except ValueError:
             return False
+
