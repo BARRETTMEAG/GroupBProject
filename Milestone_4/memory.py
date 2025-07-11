f4a7ec0f27e15f9454655928829ace482e77a16b
Collin Ross <collinross@Collins-MacBook-Air-4.local>
2025-07-11 11:58:29 -0600

    Refactor Memory class to use MemoryRegister objects

--- Milestone_4/memory.py
+++ Milestone_4/memory.py
@@ -1,20 +1,34 @@
+class MemoryRegister:
+    def __init__(self, value=0):
+        self.set(value)
+
+    def set(self, value):
+        if not isinstance(value, int):
+            raise ValueError("MemoryRegister must store an integer.")
+        if -9999 <= value <= 9999:
+            self.value = value
+        else:
+            self.value = value % 10000  # Wrap into valid range
+
+    def get(self):
+        return self.value
+
+    def __repr__(self):
+        return f"MemoryRegister({self.value})"
 class Memory:
     def __init__(self):
-        self.memory = [0] * 100  
+        self.memory = [MemoryRegister() for _ in range(100)]
         self.read_callback = None
         self.waiting_for_input = False
 
     def write(self, address, value):
         if 0 <= address < 100:
-            if -9999 <= value <= 9999:
-                self.memory[address] = value
-            else:
-                self.memory[address] = value % 10000
+            self.memory[address].set(value)
         else:
             raise IndexError("Memory address out of range")
 
     def read(self, address):
         if 0 <= address < 100:
-            return self.memory[address]
+            return self.memory[address].get()
         else:
             raise IndexError("Memory address out of range")
