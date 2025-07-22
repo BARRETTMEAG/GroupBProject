def423cc4c0dd44687ceaaf070f0a28075d7bf19
Z-Erich <erichzaugg@gmail.com>
2025-07-21 21:55:31 -0600

    Update uvsim.py

--- Milestone_5/uvsim.py
+++ Milestone_5/uvsim.py
@@ -2,11 +2,13 @@ from memory import Memory
 from cpu import CPU
 
 class UVSim:
-    def __init__(self, memory=None):
-        self.memory = memory or Memory()
+    def __init__(self, memory = None):
         self.cpu = CPU()
+        self.memory = memory if memory else Memory()
 
     def run(self):
         while not self.cpu.halted:
-            instr = self.memory.read(self.cpu.program_counter)
-            self.cpu.execute(instr, self.memory)
+            instruction = self.memory.read(self.cpu.program_counter)
+            self.cpu.execute(instruction, self.memory)
+        if instruction == 0 or instruction is None:
+            self.cpu.halted = True
